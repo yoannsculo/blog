a48ec9b12fba87a005de645db02c1125f8f21889
Yoann Sculo <yoann.sculo@gmail.com>
2012-02-27 23:00:46 +0100

    First commit

---
+++ _hooks/context.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim:syntax=python:sw=4:ts=4:expandtab
+
+Site.CONTEXT.blog = AttrDict(
+        url = 'http://82.235.77.75:8080/blog',
+        title = 'Yoann Sculo - Ingénieur Linux Embarqué',
+        author = 'Yoann Sculo',
+        mailto = 'yoann.sculo@gmail.com',
+        github_growl = 'http://github.com/xfire/growl',
+        keywords = 'yoann, sculo, cv, ingénieur systèmes embarqués, ingénieur, systèmes embarqués, blog,  linux, robotique, utt, UTT, linux embarqué, ARM, embarqué, cross-compilation',
+    )
+
+# Site.CONTEXT.blog = AttrDict(
+#         url = 'http://localhost/yoann/growl/tmp',
+#         title = 'Yoann Sculo',
+#         author = 'yoann',
+#         mailto = 'yoann.sculo@gmail.com',
+#         github_growl = 'http://github.com/xfire/growl',
+#         keywords = 'yoann, sculo, cv, ingénieur systèmes embarqués, ingénieur, systèmes embarqués, blog,  linux, robotique, utt, UTT, linux embarqué, ARM, embarqué, cross-compilation',
+#     )
+
+# url = 'http://192.168.0.10/yoann/growl/tmp',
+# url = 'http://sculo.fr/nyan',
